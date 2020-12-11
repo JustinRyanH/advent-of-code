1eccfe50a951d38f26218ee55d7050d101f446ec
justin <jhurstwright@mavenlink.com>
2020-12-11 13:53:39 -0800

    chore: make the target ignore more broad

--- 2020/day1/src/lib.rs
+++ 2020/day1/src/lib.rs
@@ -1,7 +1,18 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn finds_two_values_equal_2020() {
+        // pub fn find_sum_2020(values: &[u16]) -> (u16, 16)
+        let values = [
+            1721,
+            979,
+            366,
+            299,
+            675,
+            1456,
+        ];
+        assert_eq!(find_sum_2020(&values), (1721, 299))
     }
 }
